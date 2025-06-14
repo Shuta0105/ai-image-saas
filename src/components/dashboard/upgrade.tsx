8c1cc6a9b8aad9776fc09ef1e3205395f84d01eb
Shuta0105 <318121aa@gmail.com>
2025-06-14 16:57:02 +0900

    mod

--- src/components/dashboard/upgrade.tsx
+++ src/components/dashboard/upgrade.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { getUserSubscriptionStatus } from "@/lib/users";
 import { Button } from "../ui/button";
 import Link from "next/link";
-import { currentUser } from "@clerk/nextjs/server";
+import { useUser } from "@clerk/nextjs";
 
 const Upgrade = async () => {
-  const user = await currentUser();
+  const { user } = useUser();
+  const [subscriptionStatus, setSubscriptionStatus] = useState<string>("");
 
-  if(!user) {
+  if (!user) {
     return;
   }
-  const response = await getUserSubscriptionStatus(user.id);
-  const { subscriptionStatus } = await response.json();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const response = await getUserSubscriptionStatus(user.id);
+      const { subscriptionStatus } = await response.json();
+      setSubscriptionStatus(subscriptionStatus);
+    };
+    fetchUserData();
+  }, []);
+
   return (
     <>
       {subscriptionStatus === "FREE" ? (
